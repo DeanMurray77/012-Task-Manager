fc02b38fb0b551807890f5b559b3ee21b41dfe12
Dean <dean.l.murray@hotmail.com>
2019-07-04 10:37:12 -0600

    Add Querying functionality.
    Find, findOne

--- mongodb.js
+++ mongodb.js
@@ -11,13 +11,27 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
     const db = client.db(databaseName);
 
     // FIND USER
-    db.collection('users').findOne({name: 'Katie'}, (error, user)=> {
-        if(error) {
-            return console.log("Unable to find that user");
-        }
+    // db.collection('users').findOne({_id: new ObjectID('5d1be167011737457c03449b')}, (error, user)=> {
+    //     if(error) {
+    //         return console.log("Unable to query");
+    //     }
 
-        console.log(user);
-    })
+    //     if(!user) {
+    //         return console.log("No such user found");
+    //     }
+
+    //     console.log(user);
+    // })
+
+    //FIND MULTIPLE USERS
+    // db.collection('users').find({age: 42}).toArray((error, users) => {
+    //     console.log(users);
+    // })
+
+    //FIND A COUNT OF THE USERS
+    // db.collection('users').find({age: 42}).count((error, count) => {
+    //     console.log(count);
+    // })
 
 
     // INSERT ONE
@@ -51,5 +65,32 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
 
     //     console.log(result.ops);
     // })
+
+    //FIND A SPECIFIC TASK BY ID
+    db.collection('tasks').findOne({_id: new ObjectID('5d1c139151900e4806f234f9')}, (error, task) => {
+        if(error) {
+            return console.log("Unable to query task");
+        }
+
+        if(!task) {
+            return console.log("No such task found")
+        }
+
+        console.log(task);
+    })
+
+    //FIND ALL OF THE UNCOMPLETED TASKS
+    db.collection('tasks').find({completed: false}).toArray((error, tasks) => {
+        if(error) {
+            return console.log("Unable to query tasks");
+        }
+
+        if(tasks.length === 0) {
+            return console.log("No tasks found for that criteria");
+        }
+
+        console.log("Uncompleted tasks:")
+        console.log(tasks);
+    })
 
 });
