6f503c7b1f9fdc766b4baf4b7ae120355ded0ae2
Dean <dean.l.murray@hotmail.com>
2019-07-09 07:10:56 -0600

    /users route. Set Express to parse response to json

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.get('', (req, res) => {
     res.send("Nothing here, but it's working...");
+})
+
+app.post('/users', (req, res) => {
+    res.send("testing post users");
 })
 
 app.get('/tasks', (req, res) => {
