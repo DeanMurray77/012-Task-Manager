3866f9326baad8ed7ed41e8401ae1367f47d7d11
Dean <dean.l.murray@hotmail.com>
2019-07-10 07:43:19 -0600

    Get tasks and Get task singular

--- src/index.js
+++ src/index.js
@@ -48,8 +48,30 @@ app.get('/users/:id', (req, res) => {
         if(!user) {
             return res.status(404).send();
         }
-        
+
         res.send(user);
+    }).catch((e) => {
+        res.status(500).send(e);
+    })
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks);
+    }).catch((e) => {
+        res.status(500).send(e);
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id;
+
+    Task.findById(_id).then((task) => {
+        if(!task) {
+            return res.status(404).send();
+        }
+
+        res.send(task);
     }).catch((e) => {
         res.status(500).send(e);
     })
