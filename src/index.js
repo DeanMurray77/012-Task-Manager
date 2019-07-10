862b805c6dc8171dc31d31ecc3deba652892db81
Dean <dean.l.murray@hotmail.com>
2019-07-10 07:19:14 -0600

    Add get one user route.

--- src/index.js
+++ src/index.js
@@ -41,6 +41,14 @@ app.get('/users', (req, res) => {
     })
 })
 
+app.get('/users/:id', (req, res) => {
+    User.findById(req.params.id).then((user) => {
+        res.send(user);
+    }).catch((e) => {
+        res.status(500).send(e);
+    })
+})
+
 app.listen(port, () => {
     console.log("Server is up on port " + port);
 })
