9df7ac5c2756a2604a4fe5403b9956a1cc3033f4
Dean <dean.l.murray@hotmail.com>
2019-07-13 16:53:17 -0600

    Refactor deleteById to use async-await.

--- playground/promise-chaining2.js
+++ playground/promise-chaining2.js
@@ -1,11 +1,24 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
-Task.findByIdAndDelete('5d2520c68245b933312a5d5a').then((task) => {
-    console.log(task);
-    return Task.countDocuments({"completed": false});
-}).then((result) => {
-    console.log(result);
+// Promise Chaining method:
+// Task.findByIdAndDelete('5d2520c68245b933312a5d5a').then((task) => {
+//     console.log(task);
+//     return Task.countDocuments({"completed": false});
+// }).then((result) => {
+//     console.log(result);
+// }).catch((e) => {
+//     console.log(e);
+// })
+
+// Async-Await method:
+const deleteByIdAndCountIncomplete = async (id) => {
+    const task = await Task.findByIdAndDelete(id);
+    return await Task.countDocuments({"completed": false});
+}
+
+deleteByIdAndCountIncomplete('5d2a5d2bd93ea29aa081a23a').then((result) => {
+    console.log("Number of Incomplete Tasks: " + result);
 }).catch((e) => {
-    console.log(e);
+    console.log("Error: ", e);
 })
