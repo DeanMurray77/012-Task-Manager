e77048ed87e96bd1f53c678c4111f49231eefc6f
Dean <dean.l.murray@hotmail.com>
2019-07-13 16:18:12 -0600

    Comment out the promise chaining method.

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,15 +1,15 @@
 require('../src/db/mongoose');
 const User = require('../src/models/user');
 
-Promise chaining method:
-User.findByIdAndUpdate('5d2516ee80821e2feb1f15bc', {age: 1}).then((user) => {
-    console.log(user);
-    return User.countDocuments({age: 1});
-}).then((result) => {
-    console.log(`Number of documents with age 1: ${result}`);
-}).catch((e) => {
-    console.log(e);
-})
+// Promise chaining method:
+// User.findByIdAndUpdate('5d2516ee80821e2feb1f15bc', {age: 1}).then((user) => {
+//     console.log(user);
+//     return User.countDocuments({age: 1});
+// }).then((result) => {
+//     console.log(`Number of documents with age 1: ${result}`);
+// }).catch((e) => {
+//     console.log(e);
+// })
 
 //Async-Await method
 const updateUserAgeByID = async (id, newAge) => {
