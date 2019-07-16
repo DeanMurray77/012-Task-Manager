5daf60fc25b1db848ab9bfc2cdd4a8421403fd3c
Dean <dean.l.murray@hotmail.com>
2019-07-16 12:28:53 -0600

    Explore bcrypt functionality.

--- src/index.js
+++ src/index.js
@@ -21,3 +21,18 @@ app.get('', (req, res) => {
 app.listen(port, () => {
     console.log("Server is up on port " + port);
 })
+
+const bcrypt = require('bcryptjs');
+
+const myFunction = async () => {
+    const password = 'red12345';
+    const hashedPassword = await bcrypt.hash(password, 8);
+
+    console.log(password);
+    console.log(hashedPassword);
+
+    const isMatch = await bcrypt.compare('red1a2345', hashedPassword);
+    console.log(isMatch);
+}
+
+myFunction();
