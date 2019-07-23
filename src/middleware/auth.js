4f8b945dad75a628329f16d912cf8ede96d2fd14
Dean <dean.l.murray@hotmail.com>
2019-07-23 16:38:26 -0600

    If successfully able to decode the pull up the user
    Confirm that the token is correct for that user (in case they have deactivated a device),
    
    Then attach the user object to the request so that the route doesn't have to hit the database again to get ahold of the user.

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,9 +4,17 @@ const User = require('../models/user');
 const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace("Bearer ", '');
-        console.log("Token: " + token);
-        const isMatch = jwt.verify(token, 'secret');
-        console.log("Is match? " + JSON.stringify(isMatch));
+        const decoded = jwt.verify(token, 'secret'); //returns the id, iat and exp if decodes
+        const user = User.findOne({
+            _id: decoded._id,
+            'tokens.token': token
+        })
+        
+        if(!user) {
+            throw new Error();
+        }
+
+        req.user = user; //so route doesn't have to lookup the user
         next();        
     } catch (e) {
         res.status(401).send({error: 'Please authenticate'});
