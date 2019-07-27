b95f144ddca94f2838d7fcdca293070115862a96
Dean <dean.l.murray@hotmail.com>
2019-07-27 16:04:55 -0600

    cosmetic change

--- src/routers/user.js
+++ src/routers/user.js
@@ -72,7 +72,7 @@ router.get('/users/me', auth, async (req, res) => {
 })
 
 // Update user's profile
-router.patch('/users/me',auth,  async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body);
     const allowedUpdates = ["name", "email", "password", "age"];
 
