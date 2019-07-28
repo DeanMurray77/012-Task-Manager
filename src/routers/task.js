b0170b143e830f736886626a0b18bed45611a00e
Dean <dean.l.murray@hotmail.com>
2019-07-27 21:37:21 -0600

    Add authentication to 'update a task' route

--- src/routers/task.js
+++ src/routers/task.js
@@ -50,7 +50,7 @@ router.get('/tasks/:id', auth, async (req, res) => {
 })
 
 // Update a task
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
     const updates = Object.keys(req.body);
     const allowedUpdates = ['completed', 'description'];
 
@@ -68,7 +68,7 @@ router.patch('/tasks/:id', async (req, res) => {
     const _id = req.params.id;
 
     try {
-        const task = await Task.findById(_id);
+        const task = await Task.findOne({ _id, owner: req.user._id });
 
         if(!task) {
             return res.status(404).send();
