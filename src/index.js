cb719a4d6154dd43f5375e9ad09d1ba6a0e44df8
Dean <dean.l.murray@hotmail.com>
2019-08-08 07:13:15 -0600

    Remove Multer proof of concept/example code

--- src/index.js
+++ src/index.js
@@ -9,17 +9,6 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// example of using multer:
-const multer = require('multer');
-const upload = multer({
-    dest: 'images'
-});
-
-app.post('/upload', upload.single('upload'), (req, res) => {
-    res.send();
-})
-
-
 // Options
 app.use(express.json());
 app.use(userRouter);
