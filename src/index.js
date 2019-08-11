06a99baa8c2e8ae073fe81a589b25d2bb36dd1fe
Dean <dean.l.murray@hotmail.com>
2019-08-10 21:46:35 -0600

    Put Multer Expirement code into index.js so that I can play with errors

--- src/index.js
+++ src/index.js
@@ -21,3 +21,23 @@ app.get('', (req, res) => {
 app.listen(port, () => {
     console.log("Server is up on port " + port);
 })
+
+const multer = require('multer');
+
+const upload = multer({
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if(!file.originalname.match(/\.(doc|docx)$/)) {
+            return cb(new Error('Please upload a Word document'));
+        }
+
+        cb(undefined, true);
+    }
+})
+
+app.post('/upload', (req, res) => {
+    res.send();
+})
