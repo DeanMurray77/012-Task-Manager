0bb61a54b5a427d7a7c86446fe6f46d93769ae63
Dean <dean.l.murray@hotmail.com>
2019-08-12 07:49:19 -0600

    Add error handling to the production route.

--- src/routers/user.js
+++ src/routers/user.js
@@ -126,6 +126,8 @@ const upload = multer({
 // Add user avatar
 router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
     res.send();
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
 })
 
 module.exports = router;
