4a7632bcc92e647ca516b5ab5fe0f9561fe4930e
Dean <dean.l.murray@hotmail.com>
2019-08-13 06:41:56 -0600

    Save file to database rather than file system on local machine

--- src/routers/user.js
+++ src/routers/user.js
@@ -110,7 +110,6 @@ router.delete('/users/me', auth, async (req, res) => {
 })
 
 const upload = multer({
-    dest: 'avatars',
     limits: {
         fileSize: 1000000
     },
@@ -124,7 +123,10 @@ const upload = multer({
 })
 
 // Add user avatar
-router.post('/users/me/avatar', auth, upload.single('avatar'), (req, res) => {
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    req.user.avatar = req.file.buffer;
+    await req.user.save();
+    
     res.send();
 }, (error, req, res, next) => {
     res.status(400).send({ error: error.message })
