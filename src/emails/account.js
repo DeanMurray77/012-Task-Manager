308393fffb84bbabc28a7220eb8abd299803c93d
Dean <dean.l.murray@hotmail.com>
2019-08-14 07:35:22 -0600

    Set API Key. Create welcome email function.

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,3 +1,23 @@
-const config = require('../config');
+const sgMail = require('@sendgrid/mail');
 
-console.log(config.sendgridAPIKey);
+const config = require('../config'); //My method of storing the API key without having it end up on GitHub
+
+const fromEmailAddress = 'test@test.com';
+
+sgMail.setApiKey(config.sendgridAPIKey);
+
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: fromEmailAddress,
+        subject: 'Welcome to Taskify!',
+        text: `Hello, ${name}!\n\nI'm very glad to be able 
+        to welcome you to Taskify.\n\n
+        Thank you for joining, everyone here hopes that Taskify can 
+        unlock a new world of productivity for you.\n\n
+        Sincerely,\n\n
+        Dean`
+    })
+}
+
+module.exports = sendWelcomeEmail;
