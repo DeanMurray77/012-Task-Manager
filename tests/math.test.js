06b60d5fe336624673dd9929794f203c900084c6
Dean <dean.l.murray@hotmail.com>
2019-08-21 20:31:07 -0600

    Use the built in assertion methods.

--- tests/math.test.js
+++ tests/math.test.js
@@ -3,7 +3,9 @@ const math = require('../src/math');
 test('Should calculate total with tip', () => {
     const total = math.calculateTip(100, .03);
 
-    if(total !== 103) {
-        throw new Error("Total tip should be 103. Got " + total);
-    }
+    expect(total).toBe(103);
+
+    // if(total !== 103) {
+    //     throw new Error("Total tip should be 103. Got " + total);
+    // }
 })
