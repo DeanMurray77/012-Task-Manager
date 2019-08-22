e39b1b42f997c4a6cc8f98c2b43a8a1e9edb03c8
Dean <dean.l.murray@hotmail.com>
2019-08-21 21:48:46 -0600

    Add two more math functions and create tests

--- tests/math.test.js
+++ tests/math.test.js
@@ -6,8 +6,20 @@ test('Should calculate total with tip', () => {
     expect(total).toBe(103);
 })
 
-test('Calculate with no tip provided', () => {
+test('Should calculate total with no tip % provided', () => {
     const total = math.calculateTip(10);
 
     expect(total).toBe(12.5);
+})
+
+test('Should convert 32 F to 0 C', () => {
+    const celsiusTemp = math.fahrenheitToCelsius(32);
+
+    expect(celsiusTemp).toBe(0);
+})
+
+test('Should convert 0 C to 32 F', () => {
+    const fahrenheitTemp = math.celsiusToFahrenheit(0);
+
+    expect(fahrenheitTemp).toBe(32);
 })
