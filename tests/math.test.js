1bc910756292a85834cc2ad39181d0baa8306bd2
Dean <dean.l.murray@hotmail.com>
2019-08-21 21:52:41 -0600

    Refactor the tests to use the spread operator.

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,25 +1,25 @@
-const math = require('../src/math');
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require('../src/math');
 
 test('Should calculate total with tip', () => {
-    const total = math.calculateTip(100, .03);
+    const total = calculateTip(100, .03);
 
     expect(total).toBe(103);
 })
 
 test('Should calculate total with no tip % provided', () => {
-    const total = math.calculateTip(10);
+    const total = calculateTip(10);
 
     expect(total).toBe(12.5);
 })
 
 test('Should convert 32 F to 0 C', () => {
-    const celsiusTemp = math.fahrenheitToCelsius(32);
+    const celsiusTemp = fahrenheitToCelsius(32);
 
     expect(celsiusTemp).toBe(0);
 })
 
 test('Should convert 0 C to 32 F', () => {
-    const fahrenheitTemp = math.celsiusToFahrenheit(0);
+    const fahrenheitTemp = celsiusToFahrenheit(0);
 
     expect(fahrenheitTemp).toBe(32);
 })
