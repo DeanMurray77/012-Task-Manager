3542fec9cd29fb8f40c6b9dec37ef09e98745f28
Dean <dean.l.murray@hotmail.com>
2019-08-21 20:34:37 -0600

    Get ride of the non-assertion code.

--- tests/math.test.js
+++ tests/math.test.js
@@ -4,8 +4,4 @@ test('Should calculate total with tip', () => {
     const total = math.calculateTip(100, .03);
 
     expect(total).toBe(103);
-
-    // if(total !== 103) {
-    //     throw new Error("Total tip should be 103. Got " + total);
-    // }
 })
