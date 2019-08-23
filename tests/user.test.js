ddc80631a7f7f63631d570ede81450fcfa8adbfd
Dean <dean.l.murray@hotmail.com>
2019-08-23 12:51:11 -0600

    Begin experimenting with the Setup and Teardown functions

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,6 +1,14 @@
 const request = require('supertest');
 const app = require('../src/app');
 
+beforeEach(() => {
+    console.log('Before each')
+})
+
+afterEach(() => {
+    console.log('After each')
+})
+
 test('Should sign up a new user', async () => {
     await request(app).post('/users').send({
         name: 'Dean Murray',
