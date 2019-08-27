34b9c7c5f5546e92fb2b385f145c8d263d26247f
Dean <dean.l.murray@hotmail.com>
2019-08-27 11:25:25 -0600

    Test to confirm that a file is uploaded on the add avatar route.

--- tests/user.test.js
+++ tests/user.test.js
@@ -119,5 +119,8 @@ test('Should upload an avatar image', async () => {
         .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .attach('avatar', 'tests/fixtures/profile-pic.jpg');
 
-        expect(200);
+    expect(200);
+
+    const user = await User.findById(userOneId)
+    expect(user.avatar).toEqual(expect.any(Number));
 })
